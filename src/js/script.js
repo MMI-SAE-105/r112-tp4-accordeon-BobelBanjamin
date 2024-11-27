791bdec1415db9114163be8af8e50118f3d8f029
BobelBanjamin <banjamin.bobel@edu.univ-fcomte.fr>
2024-11-27 10:36:40 +0300

    java

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 // *** ACCORDEON ***
+
+document.addEventListener('DOMContentLoaded', () => {
+    const detailsElements = document.querySelectorAll('details');
+  
+    // Fonction pour fermer toutes les sections sauf celle cliquée
+    detailsElements.forEach(detail => {
+      detail.addEventListener('click', () => {
+        // Fermer toutes les autres sections
+        detailsElements.forEach(otherDetail => {
+          if (otherDetail !== detail) {
+            otherDetail.removeAttribute('open');
+          }
+        });
+      });
+    });
+  });
+  
+  
